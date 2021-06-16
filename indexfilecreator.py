b3e8e8229fd1b08fda570ffb135de6a0caeaed98
Deepak Kumar <deepak@intelspace.io>
2021-06-16 11:43:26 +0530

    index creator

---
+++ indexfilecreator.py
@@ -0,0 +1,58 @@
+  
+import json
+def split_string(string):
+  
+    # Split the string based on space delimiter
+    list_string = string.split(' ')
+      
+    return list_string
+
+def join_string(list_string):
+  
+    # Join the string based on '-' delimiter
+    string = 'T'.join(list_string)
+      
+    return string
+# Opening JSON file and loading the data
+# into the variable data
+
+def prepare_index_json(filename,indexname,jsonFilePath):
+  with open(filename) as json_file:
+    data = json.load(json_file)
+  
+    pdadata = data['data']
+    overalldata = []
+    processeddata =dict()
+    duplicatedrecords = dict()
+    print("duplicated records below")
+    for user in pdadata:
+        indexkey = user['artefact_meta_id']
+        indexline = {"index":{"_index":indexname,"_type":"_doc","_id":indexkey}}
+        overalldata.append(indexline)
+        if processeddata.get(indexkey) :
+           duplicatedrecords[indexkey]=True
+           print(indexkey)   
+        else : 
+         processeddata[indexkey]=True
+
+        overalldata.append(user)
+    if len(duplicatedrecords)==0 :
+        print('none') 
+    else:
+        print("-------")
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+            for data in overalldata:
+                jsonf.write(json.dumps(data))
+                jsonf.write('\n')
+            jsonf.write('\n')
+
+
+if __name__ == '__main__':
+    prepare_index_json('rawartefact.json','widgetartefactindex_v1','converted-stage-all.json')
+
+
+
+
+
+
+    
